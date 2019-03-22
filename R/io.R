79db53a68b351dabc08647305801020ef78c6551
Tan <ptdtan@gmail.com>
2019-03-22 14:51:18 +0700

    init read10X

---
+++ R/io.R
@@ -0,0 +1,88 @@
+#' This function was imported from Seurat R package
+#' citation: Butler et al., Nature Biotechnology 2018
+#'
+#' Load in data from 10X
+#'
+#' Enables easy loading of sparse data matrices provided by 10X genomics.
+#'
+#' @param data.dir Directory containing the matrix.mtx, genes.tsv, and barcodes.tsv
+#' files provided by 10X. A vector or named vector can be given in order to load
+#' several data directories. If a named vector is given, the cell barcode names
+#' will be prefixed with the name.
+#'
+#' @return Returns a sparse matrix with rows and columns labeled
+#'
+#' @importFrom Matrix readMM
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data_dir <- 'path/to/data/directory'
+#' list.files(data_dir) # Should show barcodes.tsv, genes.tsv, and matrix.mtx
+#' expression_matrix <- Read10X(data.dir = data_dir)
+#' signac_object = CreateSignacObject(raw.data = expression_matrix)
+#' }
+#'
+Read10X <- function(data.dir = NULL){
+  full.data <- list()
+  for (i in seq_along(data.dir)) {
+    run <- data.dir[i]
+    if (! dir.exists(run)){
+      stop("Directory provided does not exist")
+    }
+    if(!grepl("\\/$", run)){
+      run <- paste(run, "/", sep = "")
+    }
+    barcode.loc <- paste0(run, "barcodes.tsv")
+    gene.loc <- paste0(run, "genes.tsv")
+    matrix.loc <- paste0(run, "matrix.mtx")
+    if (!file.exists(barcode.loc)){
+      stop("Barcode file missing")
+    }
+    if (! file.exists(gene.loc)){
+      stop("Gene name file missing")
+    }
+    if (! file.exists(matrix.loc)){
+      stop("Expression matrix file missing")
+    }
+    data <- readMM(file = matrix.loc)
+    cell.names <- readLines(barcode.loc)
+    gene.names <- readLines(gene.loc)
+    if (all(grepl(pattern = "\\-1$", x = cell.names))) {
+      cell.names <- as.vector(
+        x = as.character(
+          x = sapply(
+            X = cell.names,
+            FUN = ExtractField, field = 1,
+            delim = "-"
+          )
+        )
+      )
+    }
+    rownames(x = data) <- make.unique(
+      names = as.character(
+        x = sapply(
+          X = gene.names,
+          FUN = ExtractField,
+          field = 2,
+          delim = "\\t"
+        )
+      )
+    )
+    if (is.null(x = names(x = data.dir))) {
+      if(i < 2){
+        colnames(x = data) <- cell.names
+      }
+      else {
+        colnames(x = data) <- paste0(i, "_", cell.names)
+      }
+    } else {
+      colnames(x = data) <- paste0(names(x = data.dir)[i], "_", cell.names)
+    }
+    full.data <- append(x = full.data, values = data)
+  }
+  full.data <- do.call(cbind, full.data)
+  return(full.data)
+}
+
