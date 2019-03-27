17578f839b8b3b1c6f40128dca4e273e405b0fee
trile965 <tri@bioturing.com>
2019-03-27 12:24:58 +0800

    basic filter

---
+++ R/filter.R
@@ -0,0 +1,45 @@
+
+
+#' Filter epxression matrix
+#'
+#' @param object Signac object
+#' @param slot Data for filtering. Default is raw.data
+#' @param min.cells The minimum number of cells for a gene. Can be a numeric variable.
+#'                  If NULL, it will be the at least 10 or the 2% of the total cells.
+#' @param min.genes The minimum number of genes for a cell. Default is 200.
+#' @param not.expressed Genes that have expression value equal or less than
+#'                      this number is considered not expressed. Default is 0.
+#' @importFrom Matrix rowSums colSums
+FilterData <- function(
+    object,
+    slot = "raw.data",
+    min.cells = NULL,
+    min.genes = 200,
+    not.expressed = 0
+) {
+
+    # Check if the object is valid for filtering
+    CheckInput <- function(object, slot) {
+        stopifnot(class(object)[1] == "Signac")
+        stopifnot(class(attr(object, slot))[1] == "dgCMatrix")
+    }
+
+    FilterGenes <- function(data, min.cells, not.expressed) {
+        if (is.null(min.cells)) {
+            min.cells <- max(10, ncol(data) * 0.02)
+        }
+        data <- data[Matrix::rowSums(data > not.expressed) >= min.cells, ]
+        return(data)
+    }
+
+    FilterCells <- function(data, min.genes, not.expressed) {
+        return(data <- data[, Matrix::colSums(data > not.expressed) >= min.genes])
+    }
+
+    CheckInput(object, slot)
+    data <- attr(object, slot)
+    data <- FilterGenes(data, min.cells, not.expressed)
+    data <- FilterCells(data, min.genes, not.expressed)
+    object@filtered.data <- data
+    return(object)
+}
