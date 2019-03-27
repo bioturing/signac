03a2a2aafff6788be1fd450e8687c71833724684
trile965 <tri@bioturing.com>
2019-03-27 12:33:59 +0800

    verbose

--- R/filter.R
+++ R/filter.R
@@ -9,13 +9,15 @@
 #' @param min.genes The minimum number of genes for a cell. Default is 200.
 #' @param not.expressed Genes that have expression value equal or less than
 #'                      this number is considered not expressed. Default is 0.
+#' @param verbose Talkative or not
 #' @importFrom Matrix rowSums colSums
 FilterData <- function(
     object,
     slot = "raw.data",
     min.cells = NULL,
     min.genes = 200,
-    not.expressed = 0
+    not.expressed = 0,
+    verbose = TRUE
 ) {
 
     # Check if the object is valid for filtering
@@ -24,15 +26,23 @@ FilterData <- function(
         stopifnot(class(attr(object, slot))[1] == "dgCMatrix")
     }
 
+    Cat <- function(meow, ...) {
+        if (meow) {
+            cat(..., '\n')
+        }
+    }
+
     FilterGenes <- function(data, min.cells, not.expressed) {
         if (is.null(min.cells)) {
             min.cells <- max(10, ncol(data) * 0.02)
         }
+        Cat(verbose, "[Signac] min.cells:", min.cells)
         data <- data[Matrix::rowSums(data > not.expressed) >= min.cells, ]
         return(data)
     }
 
     FilterCells <- function(data, min.genes, not.expressed) {
+        Cat(verbose, "[Signac] min.genes:", min.genes)
         return(data <- data[, Matrix::colSums(data > not.expressed) >= min.genes])
     }
 
@@ -40,6 +50,7 @@ FilterData <- function(
     data <- attr(object, slot)
     data <- FilterGenes(data, min.cells, not.expressed)
     data <- FilterCells(data, min.genes, not.expressed)
+    Cat(verbose, "[Signac] After filtering:", nrow(data), "genes X", ncol(data), "cells")
     object@filtered.data <- data
     return(object)
 }
