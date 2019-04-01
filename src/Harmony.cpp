c9476db1c183e404cf45d1f45371e01e26220220
Tan <ptdtan@gmail.com>
2019-04-01 14:24:08 +0700

    add harmoney

---
+++ src/Harmony.cpp
@@ -0,0 +1,53 @@
+#define ARMA_USE_CXX11
+#define ARMA_NO_DEBUG
+
+#include <RcppArmadillo.h>
+#include <RcppParallel.h>
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <assert.h>
+
+#include "CommonUtil.h"
+#include "incbeta.h"
+
+using namespace Rcpp;
+using namespace arma;
+using namespace RcppParallel;
+
+// [[Rcpp::depends(RcppParallel)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(Rhdf5lib)]]
+
+// [[Rcpp::export]]
+void HarmonyFit(const arma::sp_mat &mtx,
+                const std::vector<std::pair<std::string, int>> &cluster,
+                int cid)
+{
+    std::vector<std::pair<double,double> > res;
+    res.resize(mtx.n_rows, std::make_pair(0.0,0.0));
+    arma::sp_mat::const_col_iterator c_it;
+
+    for (int i = 0; i < mtx.n_cols; ++i) {
+        for (c_it = mtx.begin_col(i); c_it != mtx.end_col(i); ++c_it) {
+            if (cluster[i].second != cid)
+                continue;
+
+            res[c_it.row()].first += (*c_it);
+        }
+    }
+
+    int total = 0;
+    for (int i = 0 ; i < mtx.n_cols; ++i)
+        total += (cluster[i].second == cid);
+
+    for (int i = 0; i < mtx.n_rows; ++i)
+        res[i].first /= total;
+}
