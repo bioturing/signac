8cf07c5bde42227a63d9dd0c1d4c4c9974209f0e
Pham Thai Hoa <thaihoabo@gmail.com>
2019-04-01 18:19:17 +0700

    Add hivefive

--- src/Hdf5Model.h
+++ src/Hdf5Model.h
@@ -8,15 +8,11 @@
 #include <fstream>
 #include <string>
 #include "CommonUtil.h"
-#include <h5cpp/hdf5.hpp>
+#include <highfive/H5File.hpp>
 
 using namespace Rcpp;
 using namespace arma;
 using namespace RcppParallel;
-using namespace ::hdf5;
-
-using _HDF5_FILE_IMAP = std::map<std::string, file::File>;
-using _HDF5_FILE_MUTEX = std::map<std::string, std::mutex *>;
 
 namespace com {
 namespace bioturing {
@@ -31,26 +27,6 @@ public:
                   std::string &fileID);
     std::string createFile(const std::string &fileName, std::string &fileID);
     void closeFile(const std::string &filePath);
-
-    // read functions
-    bool readIntegerBlock(const std::string &fileID, const std::string &groupPath,
-                          const std::string &dataName, int64_t iOffset,
-                          int64_t iLimit, std::vector<int64_t> &arrReturnData);
-    bool readFloatBlock(const std::string &fileID, const std::string &groupPath,
-                        const std::string &dataName, int64_t iOffset,
-                        int64_t iLimit, std::vector<double> &arrReturnData);
-
-    // write functions
-    bool writeIntegerBlock(const std::string &fileID,
-                           const std::string &groupPath,
-                           const std::string &dataName,
-                           const std::vector<int64_t> &data);
-    bool writeFloatBlock(const std::string &fileID, const std::string &groupPath,
-                         const std::string &dataName,
-                         const std::vector<double> &data);
-    bool writeFloatMatrix(const std::string &fileID, const std::string &groupPath,
-                          const std::string &dataName,
-                          const arma::sp_mat &matrix);
 
 private:
     std::mutex mapMutex;
