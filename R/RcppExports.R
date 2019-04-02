19b43ed6ed0630f5b803572b0040535a50742d7a
Hy Vuong <vuonghy2442@hotmail.com.vn>
2019-04-02 12:01:05 +0700

    clean code

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,174 +2,174 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 FastComputeGCD <- function(num1, num2) {
-    .Call(`_Signac_FastComputeGCD`, num1, num2)
+    .Call('_Signac_FastComputeGCD', PACKAGE = 'Signac', num1, num2)
 }
 
 FastComputeLCM <- function(num1, num2) {
-    .Call(`_Signac_FastComputeLCM`, num1, num2)
+    .Call('_Signac_FastComputeLCM', PACKAGE = 'Signac', num1, num2)
 }
 
 FastGetCurrentDate <- function() {
-    .Call(`_Signac_FastGetCurrentDate`)
+    .Call('_Signac_FastGetCurrentDate', PACKAGE = 'Signac')
 }
 
 FastDiffVector <- function(a, b) {
-    .Call(`_Signac_FastDiffVector`, a, b)
+    .Call('_Signac_FastDiffVector', PACKAGE = 'Signac', a, b)
 }
 
 FastRandVector <- function(num) {
-    .Call(`_Signac_FastRandVector`, num)
+    .Call('_Signac_FastRandVector', PACKAGE = 'Signac', num)
 }
 
-Harmony <- function(mtx, col_idx, ordering) {
-    .Call(`_Signac_Harmony`, mtx, col_idx, ordering)
+HarmonyMarker <- function(mtx, col_idx) {
+    .Call('_Signac_HarmonyMarker', PACKAGE = 'Signac', mtx, col_idx)
 }
 
 FastMatMult <- function(mat1, mat2) {
-    .Call(`_Signac_FastMatMult`, mat1, mat2)
+    .Call('_Signac_FastMatMult', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastGetRowsOfMat <- function(mat, vec) {
-    .Call(`_Signac_FastGetRowsOfMat`, mat, vec)
+    .Call('_Signac_FastGetRowsOfMat', PACKAGE = 'Signac', mat, vec)
 }
 
 FastGetColsOfMat <- function(mat, vec) {
-    .Call(`_Signac_FastGetColsOfMat`, mat, vec)
+    .Call('_Signac_FastGetColsOfMat', PACKAGE = 'Signac', mat, vec)
 }
 
 FastGetSubMat <- function(mat, rvec, cvec) {
-    .Call(`_Signac_FastGetSubMat`, mat, rvec, cvec)
+    .Call('_Signac_FastGetSubMat', PACKAGE = 'Signac', mat, rvec, cvec)
 }
 
 FastCreateSparseMat <- function(nrow, ncol) {
-    .Call(`_Signac_FastCreateSparseMat`, nrow, ncol)
+    .Call('_Signac_FastCreateSparseMat', PACKAGE = 'Signac', nrow, ncol)
 }
 
 FastStatsOfSparseMat <- function(mat) {
-    .Call(`_Signac_FastStatsOfSparseMat`, mat)
+    .Call('_Signac_FastStatsOfSparseMat', PACKAGE = 'Signac', mat)
 }
 
 FastCreateFromTriplet <- function(vec1, vec2, vec_val) {
-    .Call(`_Signac_FastCreateFromTriplet`, vec1, vec2, vec_val)
+    .Call('_Signac_FastCreateFromTriplet', PACKAGE = 'Signac', vec1, vec2, vec_val)
 }
 
 FastConvertToSparseMat <- function(s) {
-    .Call(`_Signac_FastConvertToSparseMat`, s)
+    .Call('_Signac_FastConvertToSparseMat', PACKAGE = 'Signac', s)
 }
 
 FastConvertToTripletMat <- function(s) {
-    .Call(`_Signac_FastConvertToTripletMat`, s)
+    .Call('_Signac_FastConvertToTripletMat', PACKAGE = 'Signac', s)
 }
 
 FastSparseMatSqrt <- function(mat) {
-    .Call(`_Signac_FastSparseMatSqrt`, mat)
+    .Call('_Signac_FastSparseMatSqrt', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatMult <- function(mat1, mat2) {
-    .Call(`_Signac_FastSparseMatMult`, mat1, mat2)
+    .Call('_Signac_FastSparseMatMult', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastSparseMatAddition <- function(mat1, mat2) {
-    .Call(`_Signac_FastSparseMatAddition`, mat1, mat2)
+    .Call('_Signac_FastSparseMatAddition', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastSparseMatMultWithNum <- function(mat, k) {
-    .Call(`_Signac_FastSparseMatMultWithNum`, mat, k)
+    .Call('_Signac_FastSparseMatMultWithNum', PACKAGE = 'Signac', mat, k)
 }
 
 FastSparseMatSymmatl <- function(mat) {
-    .Call(`_Signac_FastSparseMatSymmatl`, mat)
+    .Call('_Signac_FastSparseMatSymmatl', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatTranspose <- function(mat) {
-    .Call(`_Signac_FastSparseMatTranspose`, mat)
+    .Call('_Signac_FastSparseMatTranspose', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatTrimatu <- function(mat) {
-    .Call(`_Signac_FastSparseMatTrimatu`, mat)
+    .Call('_Signac_FastSparseMatTrimatu', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatTrace <- function(mat) {
-    .Call(`_Signac_FastSparseMatTrace`, mat)
+    .Call('_Signac_FastSparseMatTrace', PACKAGE = 'Signac', mat)
 }
 
 FastConvertToDiagonalSparseMat <- function(mat) {
-    .Call(`_Signac_FastConvertToDiagonalSparseMat`, mat)
+    .Call('_Signac_FastConvertToDiagonalSparseMat', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatSquare <- function(mat) {
-    .Call(`_Signac_FastSparseMatSquare`, mat)
+    .Call('_Signac_FastSparseMatSquare', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatRepmat <- function(mat, i, j) {
-    .Call(`_Signac_FastSparseMatRepmat`, mat, i, j)
+    .Call('_Signac_FastSparseMatRepmat', PACKAGE = 'Signac', mat, i, j)
 }
 
 FastSparseMatSign <- function(mat) {
-    .Call(`_Signac_FastSparseMatSign`, mat)
+    .Call('_Signac_FastSparseMatSign', PACKAGE = 'Signac', mat)
 }
 
 FastSparseMatMultSD <- function(mat1, mat2) {
-    .Call(`_Signac_FastSparseMatMultSD`, mat1, mat2)
+    .Call('_Signac_FastSparseMatMultSD', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastSparseMatMultDS <- function(mat1, mat2) {
-    .Call(`_Signac_FastSparseMatMultDS`, mat1, mat2)
+    .Call('_Signac_FastSparseMatMultDS', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastSparseMatMultDD <- function(mat1, mat2) {
-    .Call(`_Signac_FastSparseMatMultDD`, mat1, mat2)
+    .Call('_Signac_FastSparseMatMultDD', PACKAGE = 'Signac', mat1, mat2)
 }
 
 FastGetRowOfSparseMat <- function(mat, i) {
-    .Call(`_Signac_FastGetRowOfSparseMat`, mat, i)
+    .Call('_Signac_FastGetRowOfSparseMat', PACKAGE = 'Signac', mat, i)
 }
 
 FastGetColOfSparseMat <- function(mat, j) {
-    .Call(`_Signac_FastGetColOfSparseMat`, mat, j)
+    .Call('_Signac_FastGetColOfSparseMat', PACKAGE = 'Signac', mat, j)
 }
 
 FastGetRowsOfSparseMat <- function(mat, start, end) {
-    .Call(`_Signac_FastGetRowsOfSparseMat`, mat, start, end)
+    .Call('_Signac_FastGetRowsOfSparseMat', PACKAGE = 'Signac', mat, start, end)
 }
 
 FastGetColsOfSparseMat <- function(mat, start, end) {
-    .Call(`_Signac_FastGetColsOfSparseMat`, mat, start, end)
+    .Call('_Signac_FastGetColsOfSparseMat', PACKAGE = 'Signac', mat, start, end)
 }
 
 FastGetSubSparseMat <- function(mat, rrvec, ccvec) {
-    .Call(`_Signac_FastGetSubSparseMat`, mat, rrvec, ccvec)
+    .Call('_Signac_FastGetSubSparseMat', PACKAGE = 'Signac', mat, rrvec, ccvec)
 }
 
 FastGetSubSparseMatByRows <- function(mat, rvec) {
-    .Call(`_Signac_FastGetSubSparseMatByRows`, mat, rvec)
+    .Call('_Signac_FastGetSubSparseMatByRows', PACKAGE = 'Signac', mat, rvec)
 }
 
 FastGetSubSparseMatByCols <- function(mat, cvec) {
-    .Call(`_Signac_FastGetSubSparseMatByCols`, mat, cvec)
+    .Call('_Signac_FastGetSubSparseMatByCols', PACKAGE = 'Signac', mat, cvec)
 }
 
 FastGetSumSparseMatByRows <- function(mat, rvec) {
-    .Call(`_Signac_FastGetSumSparseMatByRows`, mat, rvec)
+    .Call('_Signac_FastGetSumSparseMatByRows', PACKAGE = 'Signac', mat, rvec)
 }
 
 FastGetSumSparseMatByCols <- function(mat, cvec) {
-    .Call(`_Signac_FastGetSumSparseMatByCols`, mat, cvec)
+    .Call('_Signac_FastGetSumSparseMatByCols', PACKAGE = 'Signac', mat, cvec)
 }
 
 FastGetSumSparseMatByAllRows <- function(mat) {
-    .Call(`_Signac_FastGetSumSparseMatByAllRows`, mat)
+    .Call('_Signac_FastGetSumSparseMatByAllRows', PACKAGE = 'Signac', mat)
 }
 
 FastGetSumSparseMatByAllCols <- function(mat) {
-    .Call(`_Signac_FastGetSumSparseMatByAllCols`, mat)
+    .Call('_Signac_FastGetSumSparseMatByAllCols', PACKAGE = 'Signac', mat)
 }
 
 FastGetMedianSparseMatByAllRows <- function(mat) {
-    .Call(`_Signac_FastGetMedianSparseMatByAllRows`, mat)
+    .Call('_Signac_FastGetMedianSparseMatByAllRows', PACKAGE = 'Signac', mat)
 }
 
 FastGetMedianSparseMatByAllCols <- function(mat) {
-    .Call(`_Signac_FastGetMedianSparseMatByAllCols`, mat)
+    .Call('_Signac_FastGetMedianSparseMatByAllCols', PACKAGE = 'Signac', mat)
 }
 
