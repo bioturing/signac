31573d13fa247ec8e0d4b77b8589af61dccdc585
Tan Phan <tan@bioturing.com>
2019-06-30 15:19:16 +0700

    update the testing kit

--- tests/testthat/test-io.R
+++ tests/testthat/test-io.R
@@ -22,6 +22,6 @@ test_that("ReadSpMt", {
     h5.path <- system.file("extdata", "v3input.h5", package = "Signac")
     group.name = "matrix"
     auto.update = TRUE
-    mat <- Signac::ReadSpMt( h5.path, group.name, auto.update)
+    mat <- ReadSpMt( h5.path, group.name)
     expect_equal("dgCMatrix" %in%  class(mat), TRUE)
 })
